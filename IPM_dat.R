00df66805dcd0700fe48409efec5ef829d82c008
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-04-19 08:47:37 -0230

    Second commit(test) - push failed last time.

---
+++ IPM_dat.R
@@ -0,0 +1,35 @@
+
+# Set up a project - see the below link fir directions.
+#https://happygitwithr.com/rstudio-git-github.html
+
+# But basically:
+# 1.	Set up a Git repo on GitHub.
+# 2.	Create the project in R - New Project - VErsion Control - Git
+# 3. type "git add -A" in the terminal
+# 4.	Create a bunch of directories automatically (see below)
+# 5. Copy git -ignore file
+
+#Create a "name_dat.R" file
+#put this file in the folder with the project and create the following subfolders
+if(!dir.exists("archive"))dir.create("archive")
+if(!dir.exists("data"))dir.create("data")
+if(!dir.exists("figs"))dir.create("figs") #for publication quality only
+if(!dir.exists("output"))dir.create("output") # for tables and figures
+if(!dir.exists("ms"))dir.create("ms") # manuscript
+if(!dir.exists("report"))dir.create("report") #for rmd report
+if(!dir.exists("refs"))dir.create("refs") #for rmd report
+
+
+## Start----
+# shouldn't need the above after the first day
+#libraries
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(plotly)
+library(purrr)
+
+# Source files
+source("simpleLRP_FUN.R")
+save <- "no"
