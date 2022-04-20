145ca213fa4e6a0e720edaae1e61ebb1b714f477
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-04-20 20:43:16 -0230

    Major changes:  made existing figs easier to send to the dashboard and as plotly, added LD figs. Added EDA for priors.

--- IPM_EDA.R
+++ IPM_EDA.R
@@ -1,4 +1,6 @@
 ## Start----
+#libraries
+
 
 rm(list=ls())
 # load data----
@@ -10,7 +12,8 @@ source("IPM_fun.R")
 ##disaggregated data----
 
 # age breakdown by strata
-ggplot(data = df_dis_strata, aes(x = year, y = abundance, colour = stratum)) + geom_point() + facet_grid(age ~ .)
+p01 <- ggplot(data = df_dis_strata, aes(x = year, y = abundance, colour = stratum)) + geom_point() + facet_grid(age ~ .)
+p01
 
 # strata breakdown by age
 df_dis_strata %>% filter(stratum == "A"|stratum == "B"|stratum == "C") %>%
@@ -46,26 +49,31 @@ check3 <- left_join(check1, check2, by = "year") %>%
      mutate((biomass.y/biomass.x)*100)
 check3 
 
+# these are equivalent
+cbind(jd$LD, lag(jd$LD,2), jd$I2)
+cbind(jd$LD, jd$LD, lead(jd$I2,2))
 
 # this just shows that there's no real relation between any of the age classes and therefore, the Schaekell approach won't work very well.
-p <- ggplot(data = df_dis_tab, aes(x = I2, y = lead(I3))) + geom_point()
-p
-p <- ggplot(data = df_dis_tab, aes(x = I2, y = lead(I4))) + geom_point()
-p
-p <- ggplot(data = df_dis_tab, aes(x = I2, y = lead(I4, 2))) + geom_point()
-p
-p <- ggplot(data = df_dis_tab, aes(x = I2, y = I)) + geom_point()
-p
-p <- ggplot(data = df_dis_tab, aes(x = I2, y = lead(I))) + geom_point()
-p
-p <- ggplot(data = df_dis_tab, aes(x = I3, y = lead(I4))) + geom_point()
-p
-p <- ggplot(data = df_dis_tab, aes(x = I3, y = lead(I))) + geom_point()
-p
+p1 <- ggplot(data = df_dis_tab, aes(x = I2, y = lead(I3))) + geom_point()
+p1
+p2 <- ggplot(data = df_dis_tab, aes(x = I2, y = lead(I4))) + geom_point()
+p2
+p3 <- ggplot(data = df_dis_tab, aes(x = I2, y = lead(I4, 2))) + geom_point()
+p3
+p4 <- ggplot(data = df_dis_tab, aes(x = I2, y = I)) + geom_point()
+p4
+p5 <- ggplot(data = df_dis_tab, aes(x = I2, y = lead(I))) + geom_point()
+p5
+p8 <- ggplot(data = df_dis_tab, aes(x = I2, y = lead(I, 2))) + geom_point()
+p8
+p6 <- ggplot(data = df_dis_tab, aes(x = I3, y = lead(I4))) + geom_point()
+p6
+p7 <- ggplot(data = df_dis_tab, aes(x = I3, y = lead(I))) + geom_point()
+p7
 
 # I2 v I3
-p <- ggplot(jd, aes(x = log10(I2) , y = lead(log10(I3), 1), text = paste("year: ", year, "\n", sep = ""))) + geom_point()
-ggplotly(p)
+#p <- ggplot(jd, aes(x = log10(I2) , y = lead(log10(I3), 1), text = paste("year: ", year, "\n", sep = ""))) + geom_point()
+#ggplotly(p)
 
 #library(ggcorrplot)
 #corr <- round(cor(df_dis_tab[, 2:6], use = "complete.obs", method = c("pearson")), 2)
@@ -78,19 +86,29 @@ ggplotly(p)
 df_dis_summF <- df_dis_summ %>%
      filter(age !=1)
 
-p <- ggplot(df_dis_summF, aes(x = year, y = abun, fill = age))
-p <- p + geom_bar(stat = "identity")
-p
+p21 <- ggplot(df_dis_summF, aes(x = year, y = abun, fill = age))
+p21 <- p21 + geom_bar(stat = "identity")
+p21
 
-p <- ggplot(df_dis_summF, aes(x = year, y = biomass, fill = age))
-p <- p + geom_bar(stat = "identity")
-p
+p22 <- ggplot(df_dis_summF, aes(x = year, y = biomass, fill = age))
+p22 <- p22 + geom_bar(stat = "identity")
+p22
 
 ## maturity ----
 # plot maturity data
 plot(df_mat$year, df_mat$mat)
-ggplot(df_mat, aes(x = year, y = mat)) + geom_point() + theme_bw()
 
+p60 <- ggplot(jdy, aes(x = year, y = m, text = paste("I2: ", I2, "\n", sep = ""))) + geom_point() + theme_bw()
+
+p61 <- ggplot(jdy, aes(x = I2, y = m, text = paste("year: ", year, "\n", sep = ""))) + geom_point() + theme_bw()
+summary(lm(m ~ I2, data = jd))
+
+p62 <- ggplot(jdy, aes(x = I3, y = m, text = paste("year: ", year, "\n", sep = ""))) + geom_point() + theme_bw()
+p63 <- ggplot(jdy, aes(x = log(exp(I2) + exp(I3)), y = m, text = paste("year: ", year, "\n", sep = ""))) + geom_point() + theme_bw()
+
+p64 <- ggplot(jdy, aes(x = m, y = lead(I3), text = paste("year: ", year, "\n", sep = ""))) + geom_point() + theme_bw()
+
+summary(lm(lead(I3,1) ~ m, data = jdy))
 
 ## larval density ----
 # graph larval data time series
@@ -102,15 +120,51 @@ range(df_ld$larvae, na.rm = T)
 # 2018 doesn't match but condition the best ever that year, 2021 doesn't match but survey incomplete, 2010 doesn't match but survey known to be low.keith
 jd$year <- 1999:2023
 
-ggplot(jd, aes(x = lag(LD,2) , y = I2)) + geom_point()
+p10 <- ggplot(jd, aes(x = lag(LD,2) , y = I2)) + geom_point()
 
 # LD v I2
-p <- ggplot(jd, aes(x = lag(log10(LD),2) , y = log10(I2), text = paste("year: ", year, "\n", sep = ""))) + geom_point()
-ggplotly(p)
+p11 <- ggplot(jdy, aes(x = lag(log10(LD),2) , y = log10(I2), text = paste("year: ", year, "\n", sep = ""))) + geom_point()
+ggplotly(p11)
+
+p11a <- ggplot(jd_raw, aes(x = lag(log10(LD),2) , y = log10(I2), text = paste("year: ", year, "\n", sep = ""))) + geom_point()
+ggplotly(p11a)
+
+
 
 # LD v I3
-p <- ggplot(jd, aes(x = log10(LD) , y = lead(log10(I3), 1), text = paste("year: ", year, "\n", sep = ""))) + geom_point()
-ggplotly(p)
+p12 <- ggplot(jdy, aes(x = log10(LD) , y = lead(log10(I3), 1), text = paste("year: ", year, "\n", sep = ""))) + geom_point()
+ggplotly(p12)
+
+
+
+# explore prior dist----
+
+# this is the acoustic survey divided by (1,000) and then transformed to the natural log scale 
+range(jdy$I2, na.rm = T)
+range(jdy$I3, na.rm = T)
+
+mean(jdy$I2, na.rm = T)
+mean(jdy$I3, na.rm = T)
+
+sd(jdy$I2, na.rm = T)
+sd(jdy$I3, na.rm = T)
+
+# informative priors
+x_I2 <- rnorm(1000, 9.27, 1.03)
+plot(density(x_I2)) # density approaces zero at 6 & 12
+
+
+x_I3 <- rnorm(1000, 8.23, 0.95)
+plot(density(x_I3)) # density approaces zero at 5 & 11.5
+
+# uniformative priors - based on the above work, I played around with the sd until I got wider priors - using these for N2 and N3
+x_I2u <- rnorm(1000, 9.27, 3.03)
+plot(density(x_I2u)) # density approaces zero at 0 & 16
+#So, if sd^2 = 9, then precision is 1/9
+
+x_I3u <- rnorm(1000, 8.23, 3)
+plot(density(x_I3u)) # density approaces zero at 0 & 16
+#So, if sd^2 = 9, then precision is 1/9
 
 
 
