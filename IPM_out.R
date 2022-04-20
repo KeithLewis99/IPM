079414181f06a7c44baf221fc7ed8fec6535b6f1
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-04-20 20:59:59 -0230

    Medium change: added diagnostics but only have some adapted.

--- IPM_out.R
+++ IPM_out.R
@@ -5,6 +5,7 @@ library(R2jags)
 library(ggplot2)
 
 
+
 # Start----
 rm(list=ls())
 
@@ -14,14 +15,16 @@ source("IPM_fun.R")
 source("IPM_mod.R")
 
 
+
 # Model ln scale: tice N3 mortality and INdex SE and split N2----
 # try to fix the priors ito variance
 
 # JAGS settings
-parms <- c("Sld", "N2",  "N3", "tau.proc", "tau.obs", "tau.LD", "tau.ind", "I2", "I3", "y2", "y3")
+parms <- c("Sld", "N2",  "N3", "tau.proc", "tau.obs", "tau.LD", "tau.ind", "I2", "I3", "y2", "y3", "alpha", "beta", "gamma")
 
 # MCMC settings
-ni <- 20000; nt <- 6; nb <- 5000; nc <- 3
+#ni <- 20000; nt <- 6; nb <- 5000; nc <- 3
+ni <- 200000; nt <- 30; nb <- 30000; nc <- 3
 
 # run model
 ssm26 <- jags(jags.data, parameters=parms, n.iter=ni, n.burnin = nb, n.chains=nc, n.thin=nt, model.file = textConnection(cap.v7))
@@ -72,3 +75,54 @@ tp = out$sims.list$tau.proc
 tp_med = apply(tp,2,'median') # median values of y_pred
 tp_ci = apply(tp,2,'quantile', c(0.1, 0.9)) # median values of y_pred
 
+# Diagnostics----
+##R-Diagnostics----
+source('C:/Users/lewiske/Documents/R/zuur_rcode/MCMCSupportHighstatV2.R')
+source('C:/Users/lewiske/Documents/R/zuur_rcode/HighstatLibV7.R')
+library(lattice)
+
+print(out, intervals=c(0.025, 0.975), digits = 3)
+out$mean
+
+     #overdispersion - values close to 0.5 indicate a good fit pg. 77 Zuur et al. 2013 Beginners guide to GLM and GLMM
+     mean(out$sims.list$FitNew > out$sims.list$Fit)
+     # mean = 0.546333
+
+# Asess mixing of chains to see if one MCMC goes badly Zuur et al. 2013, pg 83
+vars <- c('alpha', 'beta', 'gamma')
+     #filepath <- paste0(filepath_gen, "/recruitment_1")
+
+MyBUGSChains(out, vars)
+ggsave(MyBUGSChains(out, vars), filename = paste0("Bayesian/", filepath, "/chains.pdf"), width=10, height=8, units="in")
+
+#autocorrelation - this could be a problem!!!!
+MyBUGSACF(out, vars)
+ggsave(MyBUGSACF(out, vars), filename = paste0("Bayesian/", filepath, "/auto_corr.pdf"), width=10, height=8, units="in")
+
+# Model Validation (see Zuuer et al. 2013 for options for calculating Pearson residuals)
+# Residual diagnostics
+E1 <- out$mean$PRes # Pearson resids
+F1 <- out$mean$expY # Expected values
+N2 <- out$mean$N2   # N2 - observed values? Why do these fill in the NAs of df$ln_biomass_med???
+D <- out$mean$D     # this is SSQ - but i'm looking for Cook'sD
+
+pdf(paste0("Bayesian/", filepath, "/fit_obs.pdf"))
+par(mfrow = c(2,2), mar = c(5,5,2,2))
+plot(x=F1, y = E1, xlab = "Fitted values", ylab = "Pearson residuals")
+abline(h = 0, lty = 2)
+# see notes in Mortality model
+plot(y = N2, x = F1, xlab = "Fitted values", ylab = "Observed data") # should follow the line
+abline(coef = c(0,1), lty = 2)
+par(mfrow = c(1,1))
+dev.off()
+
+# Residuals v covariates Zuur et al. 2013, pg 59-60: look for no patterns; patterns may indicated non-linear
+pdf(paste0("Bayesian/", filepath, "/resid_covar.pdf"))
+par(mfrow = c(2,2), mar = c(5,5,2,2))
+MyVar <- c("tice.std", "meandCond_lag.std")
+df_diag <- as.data.frame(model_data)
+df_diag <- cbind(df_diag, E1)
+plot(df_diag$ST, df_diag$EI, xlab = "Surface Tows", ylab = "Pearson resids")
+plot(df_diag$PS, df_diag$EI, xlab = "Pseudo-calanus", ylab = "Pearson resids")
+par(mfrow = c(1,1))
+dev.off()
