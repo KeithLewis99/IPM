f664fcdfd1b6da84809920e98ac570033174674e
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-04-21 11:28:27 -0230

    Medium changes: extract DIC, block out some code that needs work, and produce chains and autocorrelation plots for the dashboard.

--- IPM_out.R
+++ IPM_out.R
@@ -33,6 +33,8 @@ ssm26
 # create ouput
 out <- ssm26$BUGSoutput 
 
+ssm26_dic <- out$DIC
+
 ## extract raw values from chains
 raw <- ls_out(out)
 str(raw)
@@ -42,7 +44,7 @@ calc <- ls_med(raw)
 df_calc <- do.call(rbind, calc) # this doesn't work
 write(df_calc, "out2.csv")
 str(calc)
-cbind(N2_med, N3_med, N2_med+N3_med)
+#cbind(N2_med, N3_med, N2_med+N3_med)
 
 ## figures
 # N2: observation median v process median
@@ -69,60 +71,94 @@ tmp_plot <- ipm_plot(calc)
 tmp_plot
 ggsave("tmp_plot1.pdf")
 
-# Other plots
-
-tp = out$sims.list$tau.proc
-tp_med = apply(tp,2,'median') # median values of y_pred
-tp_ci = apply(tp,2,'quantile', c(0.1, 0.9)) # median values of y_pred
+     # Other plots
+     tp = out$sims.list$tau.proc
+     tp_med = apply(tp,2,'median') # median values of y_pred
+     tp_ci = apply(tp,2,'quantile', c(0.1, 0.9)) # median values of y_pred
 
 # Diagnostics----
-##R-Diagnostics----
+#Source
 source('C:/Users/lewiske/Documents/R/zuur_rcode/MCMCSupportHighstatV2.R')
 source('C:/Users/lewiske/Documents/R/zuur_rcode/HighstatLibV7.R')
 library(lattice)
 
+filepath_gen <- "biomass_cond_ag1_2_DIC_R3" 
+filepath <- paste0(filepath_gen, "/recruitment_1")
+
+
 print(out, intervals=c(0.025, 0.975), digits = 3)
 out$mean
+
 
      #overdispersion - values close to 0.5 indicate a good fit pg. 77 Zuur et al. 2013 Beginners guide to GLM and GLMM
      mean(out$sims.list$FitNew > out$sims.list$Fit)
      # mean = 0.546333
 
+## Mixing ----     
 # Asess mixing of chains to see if one MCMC goes badly Zuur et al. 2013, pg 83
-vars <- c('alpha', 'beta', 'gamma')
-     #filepath <- paste0(filepath_gen, "/recruitment_1")
+# vars for forecast model     
+     source('C:/Users/lewiske/Documents/R/zuur_rcode/MCMCSupportHighstatV2.R')
+var1 <- c('alpha', 'beta')
+#vars1 <- c('alpha', 'beta', 'gamma', "delta", "epsilon", "sigma")
 
-MyBUGSChains(out, vars)
-ggsave(MyBUGSChains(out, vars), filename = paste0("Bayesian/", filepath, "/chains.pdf"), width=10, height=8, units="in")
+MyBUGSChains(out, var1)
+mix1 <- MyBUGSChains(out, var1)
 
-#autocorrelation - this could be a problem!!!!
-MyBUGSACF(out, vars)
-ggsave(MyBUGSACF(out, vars), filename = paste0("Bayesian/", filepath, "/auto_corr.pdf"), width=10, height=8, units="in")
+#ggsave(MyBUGSChains(out, vars1), filename = paste0("Bayesian/", filepath, "/chains-forecast.pdf"), width=10, height=8, units="in")
 
-# Model Validation (see Zuuer et al. 2013 for options for calculating Pearson residuals)
-# Residual diagnostics
-E1 <- out$mean$PRes # Pearson resids
-F1 <- out$mean$expY # Expected values
-N2 <- out$mean$N2   # N2 - observed values? Why do these fill in the NAs of df$ln_biomass_med???
-D <- out$mean$D     # this is SSQ - but i'm looking for Cook'sD
+# vars for state space and demographic vars
+vars2 <- c("Sld")
+MyBUGSChains(out, vars2)
+mix2 <- MyBUGSChains(out, vars2)
+#ggsave(MyBUGSChains(out, vars2), filename = paste0("Bayesian/", filepath, "/chains-demographic.pdf"), width=10, height=8, units="in")
 
-pdf(paste0("Bayesian/", filepath, "/fit_obs.pdf"))
-par(mfrow = c(2,2), mar = c(5,5,2,2))
-plot(x=F1, y = E1, xlab = "Fitted values", ylab = "Pearson residuals")
-abline(h = 0, lty = 2)
-# see notes in Mortality model
-plot(y = N2, x = F1, xlab = "Fitted values", ylab = "Observed data") # should follow the line
-abline(coef = c(0,1), lty = 2)
-par(mfrow = c(1,1))
-dev.off()
 
-# Residuals v covariates Zuur et al. 2013, pg 59-60: look for no patterns; patterns may indicated non-linear
-pdf(paste0("Bayesian/", filepath, "/resid_covar.pdf"))
-par(mfrow = c(2,2), mar = c(5,5,2,2))
-MyVar <- c("tice.std", "meandCond_lag.std")
-df_diag <- as.data.frame(model_data)
-df_diag <- cbind(df_diag, E1)
-plot(df_diag$ST, df_diag$EI, xlab = "Surface Tows", ylab = "Pearson resids")
-plot(df_diag$PS, df_diag$EI, xlab = "Pseudo-calanus", ylab = "Pearson resids")
-par(mfrow = c(1,1))
-dev.off()
+# vars for variances
+vars3 <- c("tau.proc", "tau.obs", "tau.LD", "tau.ind")
+MyBUGSChains(out, vars3)
+mix3 <- MyBUGSChains(out, vars3)
+#ggsave(MyBUGSChains(out, vars3), filename = paste0("Bayesian/", filepath, "/chains-variance.pdf"), width=10, height=8, units="in")
+
+
+##autocorrelation----
+MyBUGSACF(out, var1)
+autocorr1 <- MyBUGSACF(out, var1)
+#ggsave(MyBUGSACF(out, vars1), filename = paste0("Bayesian/", filepath, "/auto_corr-forecast.pdf"), width=10, height=8, units="in")
+
+MyBUGSACF(out, vars2)
+autocorr2 <- MyBUGSACF(out, vars2)
+#ggsave(MyBUGSACF(out, vars2), filename = paste0("Bayesian/", filepath, "/auto_corr-demographic.pdf"), width=10, height=8, units="in")
+
+MyBUGSACF(out, vars3)
+autocorr3 <- MyBUGSACF(out, vars3)
+#ggsave(MyBUGSACF(out, vars3), filename = paste0("Bayesian/", filepath, "/auto_corr-autocorrelation.pdf"), width=10, height=8, units="in")
+
+
+## Model Validation ----
+#(see Zuuer et al. 2013 for options for calculating Pearson residuals)
+# # Residual diagnostics
+# E1 <- out$mean$PRes # Pearson resids
+# F1 <- out$mean$expY # Expected values
+# N2 <- out$mean$N2   # N2 - observed values? Why do these fill in the NAs of df$ln_biomass_med???
+# D <- out$mean$D     # this is SSQ - but i'm looking for Cook'sD
+# 
+# pdf(paste0("Bayesian/", filepath, "/fit_obs.pdf"))
+# par(mfrow = c(2,2), mar = c(5,5,2,2))
+# plot(x=F1, y = E1, xlab = "Fitted values", ylab = "Pearson residuals")
+# abline(h = 0, lty = 2)
+# # see notes in Mortality model
+# plot(y = N2, x = F1, xlab = "Fitted values", ylab = "Observed data") # should follow the line
+# abline(coef = c(0,1), lty = 2)
+# par(mfrow = c(1,1))
+# dev.off()
+# 
+# # Residuals v covariates Zuur et al. 2013, pg 59-60: look for no patterns; patterns may indicated non-linear
+# pdf(paste0("Bayesian/", filepath, "/resid_covar.pdf"))
+# par(mfrow = c(2,2), mar = c(5,5,2,2))
+# MyVar <- c("tice.std", "meandCond_lag.std")
+# df_diag <- as.data.frame(model_data)
+# df_diag <- cbind(df_diag, E1)
+# plot(df_diag$ST, df_diag$EI, xlab = "Surface Tows", ylab = "Pearson resids")
+# plot(df_diag$PS, df_diag$EI, xlab = "Pseudo-calanus", ylab = "Pearson resids")
+# par(mfrow = c(1,1))
+# dev.off()
