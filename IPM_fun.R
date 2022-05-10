d437ee24657aac836c403c21a7b87eb2b9496cae
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-05-09 21:58:50 -0230

    Minor changes: formatting

--- IPM_fun.R
+++ IPM_fun.R
@@ -46,13 +46,14 @@
                 )
         } else if(log == "yes" & forecast == "yes"){
                 jags.data <- list(#year = df_dis_tab$year,
-                        n.occasions = length(df_dis_tab$year) + num_forecasts,
-                        I2 =  c(df_dis_tabLog$I2, rep(NA, num_forecasts)),
-                        I3 =  c(df_dis_tabLog$I3, rep(NA, num_forecasts)),
-                        m = c(df_mat$mat, rep(mean(df_mat$mat), num_forecasts)),
-                        LD = as.vector(scale(c(df_ld$larvae, rep(NA, num_forecasts)),10)),
-                        TI = as.vector(scale(c(df_ice$tice, rep(mean(df_ice$tice), num_forecasts)),10)),
-                        CO = as.vector(scale(c(df_con$meanCond, rep(mean(df_con$meanCond), num_forecasts))))
+            n.occasions = length(df_dis_tab$year) + num_forecasts,
+            I2 =  c(df_dis_tabLog$I2, rep(NA, num_forecasts)),
+            I3 =  c(df_dis_tabLog$I3, rep(NA, num_forecasts)),
+            I4 =  c(df_dis_tabLog$I4, rep(NA, num_forecasts)),
+            m = c(df_mat$mat, rep(mean(df_mat$mat), num_forecasts)),
+            LD = as.vector(scale(c(df_ld$larvae, rep(NA, num_forecasts)),10)),
+            TI = as.vector(scale(c(df_ice$tice, rep(mean(df_ice$tice), num_forecasts)),10)),
+            CO = as.vector(scale(c(df_con$meanCond, rep(mean(df_con$meanCond), num_forecasts))))
                 )
         }
         return(jags.data)
