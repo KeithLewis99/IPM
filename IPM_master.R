440859ae1721dab93c0d4ac9e1a61873a37a76f5
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-05-16 11:38:07 -0230

    First commit.  This file is meant to create the dashboards in new and seperate output folders.Haven't got the loop working yet.

---
+++ IPM_master.R
@@ -0,0 +1,16 @@
+
+# The idea here is to have a loop for each model variation and then send the results to a new dashboard. I can get it to loop but not make different versions of the dashboard.  For right now, just change "b" here and in IPM_out and it creates the dashboard with all the figures in new folders.  
+
+b <- c(1:3)
+b = 2
+
+for (i in b){
+     outfile <- paste0("IPM_out_diag", i, ".html")
+     outDir1 <- paste0("output/output", i)
+     outDir2 <- paste0("output/output_ez", i)
+     #if(!dir.exists(paste0("output/", outDir)))dir.create(outDir)
+     rmarkdown::render("IPM_out_diag.Rmd", output_file = outfile, output_dir = outDir1) # makes a dashboard
+     ezknitr::ezknit(file = "IPM_out_diag.Rmd", out_dir = outDir2, keep_md = F) # saves all graphs as png in folder.
+}
+
+
