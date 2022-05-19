4e6c12b5ccf499c81188d0ff2f9b72b820cf6457
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-05-18 22:29:50 -0230

    Minor change: added EDA to the master file.

--- IPM_master.R
+++ IPM_master.R
@@ -1,8 +1,9 @@
 
 # The idea here is to have a loop for each model variation and then send the results to a new dashboard. I can get it to loop but not make different versions of the dashboard.  For right now, just change "b" here and in IPM_out and it creates the dashboard with all the figures in new folders.  
 
+# IPM_out_diag----
 b <- c(1:3)
-b = 2
+b = 1
 
 for (i in b){
      outfile <- paste0("IPM_out_diag", i, ".html")
@@ -10,7 +11,14 @@ for (i in b){
      outDir2 <- paste0("output/output_ez", i)
      #if(!dir.exists(paste0("output/", outDir)))dir.create(outDir)
      rmarkdown::render("IPM_out_diag.Rmd", output_file = outfile, output_dir = outDir1) # makes a dashboard
-     ezknitr::ezknit(file = "IPM_out_diag.Rmd", out_dir = outDir2, keep_md = F) # saves all graphs as png in folder.
+     #ezknitr::ezknit(file = "IPM_out_diag.Rmd", out_dir = outDir2, keep_md = F) # saves all graphs as png in folder.
 }
 
 
+# EDA----
+
+outfile <- paste0("IPM_EDA.html")
+outDir1 <- paste0("output/outputEDA", i)
+outDir2 <- paste0("output/outputEDA_ez", i)
+#if(!dir.exists(paste0("output/", outDir)))dir.create(outDir)
+rmarkdown::render("IPM_EDA.Rmd", output_file = outfile, output_dir = outDir1) # makes a dashboard
