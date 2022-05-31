8d74400c4d3e8be0e3d07822de4f76ebb8a785dd
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-05-31 15:23:43 -0230

    Minor change: format.

--- IPM_master.R
+++ IPM_master.R
@@ -7,11 +7,11 @@ b = 2
 
 for (i in b){
      outfile <- paste0("IPM_out_diag", i, ".html")
-     outDir1 <- paste0("output/output", i)
+     outDir1 <- paste0("output/outputTEST_1999", i)
      outDir2 <- paste0("output/output_ez", i)
      #if(!dir.exists(paste0("output/", outDir)))dir.create(outDir)
      rmarkdown::render("IPM_out_diag.Rmd", output_file = outfile, output_dir = outDir1) # makes a dashboard
-     ezknitr::ezknit(file = "IPM_out_diag.Rmd", out_dir = outDir2, keep_md = F) # saves all graphs as png in folder.
+     #ezknitr::ezknit(file = "IPM_out_diag.Rmd", out_dir = outDir2, keep_md = F) # saves all graphs as png in folder.
 }
 
 
