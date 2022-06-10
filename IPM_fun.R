05667bc6a7bc78f40098f40a26869b1777c92107
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-06-09 23:41:19 -0230

    Minor but fixed an error in the prediciton interval.

--- IPM_fun.R
+++ IPM_fun.R
@@ -138,7 +138,7 @@ ls_med <- function(ls){
   # calculate the credible interval for all ages by year
   N_ci <- apply(log(exp(ls$N3)+exp(ls$N2) + exp(ls$N4)), 2, 'quantile', c(0.025, 0.975))
   # calculate the prediction interval based on simulated data using the approach in Schuab and Kerry
-  Pr_ci <- log(apply(exp(ls$I3.rep)+exp(ls$I2.rep) + exp(ls$N4), 2, 'quantile', c(0.1, 0.9)))
+  Pr_ci <- log(apply(exp(ls$I3.rep)+exp(ls$I2.rep) + exp(ls$I4.rep), 2, 'quantile', c(0.1, 0.9)))
   
   ls_calc <- list(ls_med = ls_med, ls_cri = ls_cri, ls_pri = ls_pri, N_med = N_med, N_ci = N_ci, Pr_ci = Pr_ci)
      return(ls_calc)
