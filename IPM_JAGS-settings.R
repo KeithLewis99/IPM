fe3aee4f6ef55307a6f0ee30ca7e2869795808bb
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-06-17 16:26:47 -0230

    Minor but important updates to parameters

--- IPM_JAGS-settings.R
+++ IPM_JAGS-settings.R
@@ -19,7 +19,8 @@ parms1 <- c("tau.proc2", "tau.proc3", "tau.proc4", "tau.obs",
 # cap.v22 is AMRAext
 ### b == 2
 parms2 <- c("tau.proc2", "tau.proc3", "tau.proc4", "tau.obs", 
-            "N2",  "N3", "N4", "eta", "zeta", "eps", "ar_mean", "ma_mean",
+            "N2",  "N3", "N4", 
+            #"eta", "zeta", "eps", "ar_mean", "ma_mean",
             "mu2", "alpha2", "beta2",  "gamma2", "delta2",
             "mu3", "alpha3", "gamma3", "delta3", "epsilon3",
             "mu4",
@@ -50,7 +51,6 @@ parms4 <- c("tau.proc2", "tau.proc3", "tau.proc4", "tau.obs",
             "Dssm.rep", "Dmape.rep",  "Tturn.rep",
             "I2.rep", "I3.rep", "I4.rep", "I.rep"
 ) 
-
 
 # # cap.v10: Model ln scale: N2-N4 + forecast for each age ----
 # # and with a rdm walk (previous one does not have forecast)
@@ -85,15 +85,15 @@ parms6 <- c("tau.proc2", "tau.proc3", "tau.proc4", "tau.obs",
 ## this has common parameters for N3:N4 for the forecast model but not N2
 # cap.v22 is AMRAext
 
-parms6 <- c("tau.proc2", "tau.proc3", "tau.proc4", "tau.obs",
-             "N2",  "N3", "N4",
-             "mu2", "beta2",  "gamma2", "delta2",
-             "mu3", "alpha3", "gamma3", "delta3", "epsilon3",
-             "mu4",
-             "Dssm.obs", "Dmape.obs",  "Tturn.obs",
-             "Dssm.rep", "Dmape.rep",  "Tturn.rep",
-             "I2.rep", "I3.rep", "I4.rep", "I.rep"
-)
+#parms6 <- c("tau.proc2", "tau.proc3", "tau.proc4", "tau.obs",
+             # "N2",  "N3", "N4",
+             # "mu2", "beta2",  "gamma2", "delta2",
+             # "mu3", "alpha3", "gamma3", "delta3", "epsilon3",
+             # "mu4",
+             # "Dssm.obs", "Dmape.obs",  "Tturn.obs",
+             # "Dssm.rep", "Dmape.rep",  "Tturn.rep",
+             # "I2.rep", "I3.rep", "I4.rep", "I.rep"
+#)
 
 # cap.v31: Model ln scale: N2-N4 + forecast for each age
 ## trying matrix model approach with two loops.
@@ -159,12 +159,23 @@ if (b==1){ # model with separate parms for each age
      vars_N2 <- c("u")
      vars_N3 <- c("mu3[10]", "alpha3", "gamma3", "delta3", "epsilon3", "mu4[10]")
      vars_N4 <- c(NA)
+} else if (b==5) { # model separate parms for N2 and N3:N4
+        parms = parms5
+        tC = cap.v28
+        tC.txt = "cap.v28"
+        vars_vAR <- c("tau.proc2", "tau.proc3", "tau.proc4", "tau.obs")
+        vars_Nyear <- c("N2[10]", "N3[10]", "N4[10]")
+        vars_N2 <- c("u")
+        vars_N3 <- c("mu3[10]", "alpha3", "gamma3", "delta3", "epsilon3", "mu4[10]")
+        vars_N4 <- c(NA)     
 } else if (b==6) { # model separate parms for N2 and N3:N4  - EXT
         parms = parms2
         # tC = cap.v8
         # tC.txt = "cap.v8"
-        tC = cap.v26
-        tC.txt = "cap.v26"
+        #tC = cap.v26
+        #tC.txt = "cap.v26"
+        tC = cap.v27
+        tC.txt = "cap.v27"
         vars_vAR <- c("tau.proc2", "tau.proc3", "tau.proc4", "tau.obs")
         vars_Nyear <- c("N2[10]", "N3[10]", "N4[10]")
         vars_N2 <- c("mu2[10]","alpha2", "beta2",  "gamma2", "delta2")
