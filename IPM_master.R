935a980bb44ac95dca45cad498dca90fdfe257d7
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2022-09-07 22:15:40 -0230

    minor: added notes so the folder designation system is standardized

--- IPM_master.R
+++ IPM_master.R
@@ -3,12 +3,17 @@
 
 # IPM_out_diag----
 b <- c(1:3)
-b = 2
+b = 10
 
-# folder files should follow this format: model #, time, description
+# folder files should follow this format: model 
+     #, start/end of time series, 
+     # number iterations, 
+     # description - FC = forecast, AR  = auto-regression, MA = model average, 
+     # number of time periods (3TP = 3 time periods)
+     # i = b, i.e., the info in IPM_JAGS-settings
 for (i in b){
      outfile <- paste0("IPM_out_diag", i, ".html")
-     outDir1 <- paste0("output/M29_1985P_20K_CPreg_b=", i)
+     outDir1 <- paste0("output/M38rev_1985P_5M_FC_3TP", i)
      #outDir2 <- paste0("output/output_ez", i)
      #if(!dir.exists(paste0("output/", outDir)))dir.create(outDir)
      rmarkdown::render("IPM_out_diag.Rmd", output_file = outfile, output_dir = outDir1) # makes a dashboard
