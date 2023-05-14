ae1401c01cc0a2f8e023d223057e33ca5cccd452
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2023-05-14 20:45:16 -0230

    Major: moved cohort maturity, Z&M, and mature biomass from _dat to _EDA file

--- IPM_EDA.R
+++ IPM_EDA.R
@@ -189,3 +189,170 @@ str(df_aaa)
 #          xlab = "Year", ylab = "Capelin abundance (billions)",
 #          filename = "figs/2-cond-rank-year.pdf", save = "no")
 
+# Cohort-maturity----
+# create data frame to show realationships between mature/imm age 2, age 3 and age 4
+tmp1 <- as.data.frame(cbind(year = 1985:2021, age2 = jags.data.m$matI[1:37,1], perMat = jags.data.m$matM[1:37,1]))
+tmp1$imm <- log(exp(tmp1$age2)*(1-tmp1$perMat))
+tmp1$mat <- log(exp(tmp1$age2)*tmp1$perMat)
+tmp <- as.data.frame(cbind(tmp1, age3 = lead(jags.data.m$matI[1:37,2]), age4 = lead(jags.data.m$matI[1:37,3], 2)))
+
+tmp_long <- pivot_longer(tmp, cols = c("mat", "imm", "age3", "age4"))
+
+level_order <- c("mat", "imm", "age3", "age4")
+tmp_long$name <- factor(tmp_long$name, levels=level_order)
+
+# whole time series
+p <- ggplot(data = tmp_long, aes (x = year, y = exp(value), fill = factor(name, level_order)))
+p <- p + geom_bar(stat="identity", width = 1, position=position_dodge())
+p <- p + scale_fill_manual(values = c("black", "lightgoldenrod2", "darkgreen", "red"))
+p <- p + guides(fill=guide_legend(title="Age/Mat"))
+p <- p + theme_bw()
+p
+#ggsave("figs/recruitment1986_2020.png", width = 7, height = 5)
+
+tmp$check <- tmp$age2-tmp$age3
+
+
+# post collapse
+p <- ggplot(data = tmp_long[25:148,], aes (x = year, y = exp(value), fill = factor(name, level_order)))
+p <- p + geom_bar(stat="identity", position=position_dodge())
+p <- p + scale_fill_manual(values = c("black", "lightgoldenrod2", "darkgreen", "red"))
+p <- p + guides(fill=guide_legend(title="Age/Mat"))
+p <- p + theme_bw()
+p
+#ggsave("figs/recruitment1990_2020.png", width = 7, height = 5)
+
+
+# post collapse with Recovery for maturity and q
+## even with these adjustments, there are years where age 2 < age 3
+tmp1$mat <- log(exp(tmp1$mat)*0.26)
+tmp1$imm <- log(exp(tmp1$imm)/0.5)
+tmp <- as.data.frame(cbind(tmp1, age3 = lead(jags.data.m$matI[1:37,2]), age4 = lead(jags.data.m$matI[1:37,3], 2)))
+tmp_long <- pivot_longer(tmp, cols = c("mat", "imm", "age3", "age4"))
+level_order <- c("mat", "imm", "age3", "age4")
+tmp_long$name <- factor(tmp_long$name, levels=level_order)
+
+p <- ggplot(data = tmp_long[25:148,], aes (x = year, y = exp(value), fill = factor(name, level_order)))
+p <- p + geom_bar(stat="identity", position=position_dodge())
+p <- p + scale_fill_manual(values = c("black", "lightgoldenrod2", "darkgreen", "red"))
+p <- p + guides(fill=guide_legend(title="Age/Mat"))
+p <- p + theme_bw()
+p
+#ggsave("figs/recruitment1990_2020_modified.png", width = 7, height = 5)
+
+
+# redo but with the age2 in as well.
+tmp_long <- pivot_longer(tmp, cols = c("mat", "imm", "age2", "age3", "age4"))
+str(tmp_long)
+tmp_long$name <- as.factor(tmp_long$name)
+tmp_long$name1 <- as.factor(tmp_long$name)
+
+level_order <- c("age2", "mat", "imm", "age3", "age4")
+tmp_long$name <- factor(tmp_long$name, levels=level_order)
+
+p <- ggplot(data = tmp_long, aes (x = year, y = exp(value), fill = factor(name, level_order)))
+p <- p + geom_bar(stat="identity", position=position_dodge())
+p <- p + scale_fill_manual(values = c("orange", "black", "lightgoldenrod2", "darkgreen", "red"))
+p <- p + guides(fill=guide_legend(title="Age/Mat"))
+p <- p + theme_bw()
+p
+
+
+p <- ggplot(data = tmp_long[31:185,], aes (x = year, y = exp(value), fill = factor(name, level_order)))
+p <- p + geom_bar(stat="identity", position=position_dodge())
+p <- p + scale_fill_manual(values = c("orange", "black", "lightgoldenrod2", "darkgreen", "red"))
+p <- p + guides(fill=guide_legend(title="Age/Mat"))
+p <- p + theme_bw()
+p
+
+
+# Z & M Barents Sea (BS) style----
+## See Notation.Rmd in C:\Users\lewiske\Documents\capelin_LRP\analyses\capelinLRP
+### Have asked Ale and Hannah about getting the proper equestions here.  Both have responded but do not have the actual equations.
+
+df_dis_tab$Z2 <- -log(lead(df_dis_tab$I3,1)/
+                         (df_dis_tab$I2*(1-df_mat1_per$mat2*0.01)))
+mean(df_dis_tab$Z2[df_dis_tab$Z2>0], na.rm = T)
+
+write.csv(df_dis_tab$Z2, "M.csv")
+
+df_dis_tab$Z3 <- -log(lead(df_dis_tab$I4,1)/
+                         (df_dis_tab$I3*(1-df_mat1_per$mat3*0.01)))
+mean(df_dis_tab$Z3[df_dis_tab$Z3>0], na.rm = T)
+
+#Hilborn and Walters 11.2.2
+median(-(log(lead(df_dis_tab$I3,1)) - log(df_dis_tab$I2)), na.rm = T)
+sd(-(log(lead(df_dis_tab$I3,1)) - log(df_dis_tab$I2)), na.rm = T)
+sd(-(log(lead(df_dis_tab$I3[-7],1)) - log(df_dis_tab$I2[-6])), na.rm = T)
+median(-(log(lead(df_dis_tab$I4,1)) - log(df_dis_tab$I3)), na.rm = T)
+median(-(log(lead(df_dis_tab$I5,1)) - log(df_dis_tab$I4)), na.rm = T)
+median(-(log(lead(df_dis_tab$I6,1)) - log(df_dis_tab$I5)), na.rm = T)
+
+# df_dis_tab$M <- -log((lead(df_dis_tab$I3,1) + lead(matCAA[1:37,2],1) + matCAA[1:37,1])/(df_dis_tab$I2*(1-df_mat1_per$mat2*0.01)))                    
+
+# something is wrong here.  The matCAA is supposed to be immatures but too many negatives.  The problem is that there are basically no imature age 3 so I've modified the equation
+df_dis_tab$M3 <- -log((lead(df_dis_tab$I4,1) + lead(matCAA[1:37,3],1) + matCAA[1:37,2])/(df_dis_tab$I3*(1-df_mat1_per$mat3*0.01)))  
+
+-log((lead(df_dis_tab$I4,1) + lead(matCAA[1:37,3],1) + matCAA[1:37,2])/(df_dis_tab$I3))  
+
+df_dis_tab$Mi <- -log((lead(df_dis_tab$I3,1) + lead(matCAA[1:37,2],1) + matCAA[1:37,1])/(df_dis_tab$I2))                    
+
+year <- c(1985:2021)
+
+mortTab <- df_dis_tab[, c(1, 9:12)]
+
+# remove outliers as per BS
+
+
+
+# mature biomass----
+# biomass-at-age-1985-2012 - from FRan -> put these values in ssb_calculator and then 
+df_ssb_FM <- read_csv("data/ssb.csv")
+plot(df_ssb_FM$ssb, lag(df_ssb_FM$abundance, 2))
+
+
+# pivot the data - longer to wider with the disaggregated abundance as columns
+# get abundance/biomass by age
+# these values are close to those in df_cap but not exact.  
+# 1999-present
+df_ssb_tab <- df_baa_filter[, c(1:2, 6)] %>%
+   #  filter(age != 1 & age != 5) %>%
+   filter(age != 1) %>%
+   #pivot_wider(names_from = age, values_from = biomass) %>%
+   pivot_wider(names_from = age, values_from = matbio) %>%
+   #rename(a2 = '2', a3 = '3', a4 = '4')
+   rename(ssb2 = '2', ssb3 = '3', ssb4 = '4', ssb5 = '5') %>%
+   mutate(ssb = sum(c_across(starts_with("s")), na.rm = T)) %>%
+   mutate(var = var(c_across(starts_with("s")), na.rm = T))  %>%
+   mutate(sd = sd(c_across(starts_with("s")), na.rm = T))
+df_ssb_tab
+
+#incredibly, I can't figure out how to get pivot_wider to fill in the missing years with NA!!!  So using this crude but proven method
+df_tmp <- df_ssb_tab[1:3,]
+df_tmp[, 1:8] <- NA
+df_tmp$year[1:3] <- c(2006, 2016, 2020)
+df_tmp
+
+# bind the blank years (NAs) with the data
+df_ssb_tab <- bind_rows(df_tmp, df_ssb_tab) %>% 
+   arrange(year)
+df_ssb_tab$ssb6 <- NA
+df_ssb_tab <- df_ssb_tab[,c(1:5, 9, 6)]
+str(df_ssb_tab, give.attr = F)
+
+srr_1998 <- left_join(df_ssb_tab, df_dis_tab, by = "year") %>%
+   rename(abundance = I2) %>%
+   mutate_at(vars(abundance), ~ ./ 1000)
+
+
+# combine the data sets as needed.
+if(disaggregated == "1985-present") {
+   df_ssb <- rbind(df_ssb_FM[1:14,], srr_1998[,c(1,9,7)])
+} else {
+   df_ssb
+} 
+
+
+df_ssb$abundance_tp2 <- lead(df_ssb$abundance,2)
+
+write.csv(df_ssb, "data/ssb_all.csv")
