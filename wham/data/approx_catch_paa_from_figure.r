ab1f8c9506bfadbe48988a61e214da3773d10ac9
PaulRegular <Paul.Regular@dfo-mpo.gc.ca>
2024-09-30 07:31:47 -0230

    Tweak digitization of catch paa

--- wham/data/approx_catch_paa_from_figure.r
+++ wham/data/approx_catch_paa_from_figure.r
@@ -1,14 +1,18 @@
 
 library(dplyr)
 
-## Cumulative PAA
-approx_paa <- readxl::read_xlsx("wham/data/approx_catch_paa_from_figure.xlsx", sheet = "approx_data")
+## Cumulative PAA extracted from Figure 6 in Research Document 2023/076 
+## using WebPlotDigitizer (https://automeris.io/wpd/)
+approx_paa <- read.csv("wham/data/approx_catch_paa_from_figure.csv")
 
 approx_paa$year <- round(approx_paa$year)
 approx_paa <- approx_paa |> 
     group_by(year, age) |> 
     summarise(prop = mean(prop)) |> 
-    filter(year <= 2018)
+    filter(year <= 2019)
+
+approx_paa <- rbind(data.frame(year = sort(unique(approx_paa$year)), age = 1, prop = 0),
+                    approx_paa)
 
 approx_paa_mat <- xtabs(prop ~ year + age, data = approx_paa)
 ind5 <- approx_paa_mat[, "5"] == 0
@@ -21,7 +25,7 @@ pre_collapse_means <- post_collapse_means <- rep(0, ncol(approx_paa_mat))
 
 approx_paa_mat <- rbind(t(replicate(8, pre_collapse_means)),
                         approx_paa_mat,
-                        t(replicate(4, post_collapse_means)))
+                        t(replicate(3, post_collapse_means)))
 rownames(approx_paa_mat) <- 1972:2022
 
 approx_paa_mat <- cbind(0, t(apply(approx_paa_mat, 1, diff)))
